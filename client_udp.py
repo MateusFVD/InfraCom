35d11d22d5a99df8797e8fb123d87252594bd8a2
Luís Paulo Trevisan <lpst@cin.ufpe.br>
2025-07-02 21:42:35 -0300

    refactor: variable name and exception

--- client_udp.py
+++ client_udp.py
@@ -1,57 +1,59 @@
 import socket
 import os
 
-HOST = "127.0.0.1"  #Endereço IP do servidor
-PORT = 5000         #Porta do servidor
+HOST = "127.0.0.1"  # Endereço IP do servidor
+PORT = 5000  # Porta do servidor
 dest = (HOST, PORT)
 
-#Cria um socket UDP (SOCK_DGRAM) usando IPv4 (AF_INET).
+# Cria um socket UDP (SOCK_DGRAM) usando IPv4 (AF_INET).
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#-------------ENVIO DO ARQUIVO -------------------------------------
+# -------------ENVIO DO ARQUIVO -------------------------------------
 
-file_src = "MonaLisa.jpg"  #Nome do arquivo a ser enviado
+file_src = "MonaLisa.jpg"  # Nome do arquivo a ser enviado
 
 try:
-    file_size = os.path.getsize(file_src)  #Obtém o tamanho do arquivo em bytes
+    file_size = os.path.getsize(file_src)  # Obtém o tamanho do arquivo em bytes
     print(f"O tamanho do arquivo é : {file_size} bytes")
 
-    #Converte o nome do arquivo para bytes e envia para o servidor.
+    # Converte o nome do arquivo para bytes e envia para o servidor.
     clientSocket.sendto(file_src.encode(), dest)
 
     # Usar o "with", abre o arquivo em modo de leitura e garante que será fechado automaticamente
     with open(file_src, "rb") as file:
         while True:
-            file_data = file.read(1024)  #Lê 1024 bytes do arquivo
+            file_data = file.read(1024)  # Lê 1024 bytes do arquivo
             if not file_data:
-                #Quando o arquivo é lido por completo, envia "<END>" para indicar o fim.
+                # Quando o arquivo é lido por completo, envia "<END>" para indicar o fim.
                 clientSocket.sendto(b"<END>", dest)
                 break
-            clientSocket.sendto(file_data, dest)  #Lê e envia partes do arquivo em segmentos de 1024 bytes.
+            clientSocket.sendto(
+                file_data, dest
+            )  # Lê e envia partes do arquivo em segmentos de 1024 bytes.
     print("Arquivo enviado com sucesso.")
-#Tratamento de erro e exceção
-except fileNotFoundError:
+# Tratamento de erro e exceção
+except FileNotFoundError:
     print(f"Erro: O arquivo {file_src} não foi encontrado.")
     clientSocket.close()
-    exit() #Encerra o script se o arquivo não existir
+    exit()  # Encerra o script se o arquivo não existir
 
-#---------- RECEBENDO O ARQUIVO DE VOLTA ---------------------------
+# ---------- RECEBENDO O ARQUIVO DE VOLTA ---------------------------
 
 file_bytes = b""
 
-#Recebe o nome do arquivo modificado de volta do servidor
-file_name, clientAddress = clientSocket.recvfrom(1024)
+# Recebe o nome do arquivo modificado de volta do servidor
+file_name, serverAddress = clientSocket.recvfrom(1024)
 file_name = file_name.decode()
 file_name = "client_" + file_name
-file = open(file_name, "wb")  #Abre um novo arquivo binário para escrita
+file = open(file_name, "wb")  # Abre um novo arquivo binário para escrita
 
-dcount = 0  #Contador para contagem dos datagramas recebidos
+dcount = 0  # Contador para contagem dos datagramas recebidos
 while True:
     datagram, serverAddress = clientSocket.recvfrom(1024)  # Recebe um datagrama
     dcount += 1
-    print(f"datagram {dcount} recebido do servidor ({clientAddress})")
+    print(f"datagram {dcount} recebido do servidor ({serverAddress})")
 
-    if datagram == b"<END>":  #Recebe os datagramas até encontrar o marcador "<END>".
+    if datagram == b"<END>":  # Recebe os datagramas até encontrar o marcador "<END>".
         dcount = 0
         break
     else:
@@ -59,6 +61,6 @@ while True:
 
 print("devolucao finalizada.")
 
-file.write(file_bytes)  #Escreve os bytes recebidos no arquivo
-file.close()  #Fecha o arquivo
-clientSocket.close()  #Fecha o socket do cliente
+file.write(file_bytes)  # Escreve os bytes recebidos no arquivo
+file.close()  # Fecha o arquivo
+clientSocket.close()  # Fecha o socket do cliente
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-import socket 
+import socket
 import os
 
-#----------definindo configurações-------------
+# ----------definindo configurações-------------
 HOST = "127.0.0.1"
 PORT = 5000
-BUFFER_SIZE = 1024 
+BUFFER_SIZE = 1024
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((HOST, PORT))
@@ -15,11 +15,15 @@ while True:
     print("\nAguardando um cliente se conectar...")
 
 try:
-    file_name_bytes, client_address = server_socket.recvfrom(BUFFER_SIZE) # recebe o nome do arquivo em bytes e o endereço de quem enviou (cliente)!! necessário para retornar o arquivo
-    
-    file_name = file_name_bytes.decode() # decodificando para string
-    server_file_name = "server_" + file_name # cria o nome do arquivo que será salvo no servidor!
-    
+    file_name_bytes, client_address = server_socket.recvfrom(
+        BUFFER_SIZE
+    )  # recebe o nome do arquivo em bytes e o endereço de quem enviou (cliente)!! necessário para retornar o arquivo
+
+    file_name = file_name_bytes.decode()  # decodificando para string
+    server_file_name = (
+        "server_" + file_name
+    )  # cria o nome do arquivo que será salvo no servidor!
+
     print(f"Conexão recebida de: {client_address}")
     print(f"Nome do arquivo a ser recebido: '{file_name}'")
     print(f"Salvando como: '{server_file_name}'")
@@ -35,10 +39,11 @@ try:
                 print("Sinal de <END> recebido. O arquivo foi completamente recebido.")
                 break
 
-            #escreve um pedaço recebido no arquivo
+            # escreve um pedaço recebido no arquivo
             f.write(data)
 
     print("Arquivo salvo com sucesso no servidor.")
 
 except Exception as e:
     print(f"Ocorreu um erro durante a recepção: {e}")
+
